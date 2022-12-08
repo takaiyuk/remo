ada0c99fa1adc5d210f602ceb6cc33251d45d56d
takaiyuk <takaishikawa1225@gmail.com>
2022-12-08 09:08:27 +0900

    Insert row when temperature updated

--- remo/spreadsheet.py
+++ remo/spreadsheet.py
@@ -1,10 +1,18 @@
+from datetime import datetime
 from pathlib import Path
 
 import gspread
+import structlog
 from gspread.worksheet import Worksheet
 from oauth2client.service_account import ServiceAccountCredentials
 
 from remo.remo import DeviceTemperature
+
+logger = structlog.get_logger(__name__)
+
+
+def format_datetime(dt: datetime) -> str:
+    return dt.strftime("%Y-%m-%dT%H:%M:%S")
 
 
 def get_sheet_client(sheet_filename: str, secret_json_path: Path) -> Worksheet:
@@ -18,12 +26,27 @@ def get_sheet_client(sheet_filename: str, secret_json_path: Path) -> Worksheet:
     return sheet
 
 
+def is_temperature_updated(sheet_client: Worksheet, device_temperature: DeviceTemperature) -> bool:
+    sheet_values: list[list[str]] = sheet_client.get_all_values()
+    if not sheet_values:
+        return True
+    last_row = sheet_values[-1]
+    last_val = float(last_row[0])
+    last_created_at = last_row[1]
+    return last_val != device_temperature.val or last_created_at != format_datetime(
+        device_temperature.created_at
+    )
+
+
 def write_sheet(
     env_dict: dict[str, str], device_temperature: DeviceTemperature, verbose: bool = True
 ) -> None:
     sheet_client = get_sheet_client(env_dict["SHEET_FILENAME"], Path(env_dict["SECRET_JSON_PATH"]))
-    row = [device_temperature.val, device_temperature.created_at.strftime("%Y-%m-%dT%H:%M:%S")]
-    index = len(sheet_client.get_all_values()) + 1
-    sheet_client.insert_row(row, index)
-    if verbose:
-        print(row)
+    if is_temperature_updated(sheet_client, device_temperature):
+        row = [device_temperature.val, format_datetime(device_temperature.created_at)]
+        index = len(sheet_client.get_all_values()) + 1
+        sheet_client.insert_row(row, index)
+        if verbose:
+            logger.info(f"Insert row: {row}")
+    else:
+        logger.info("Temperature is not updated")
