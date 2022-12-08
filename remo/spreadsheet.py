556979b2f86a87914d11887532771efad592891f
takaiyuk <takaishikawa1225@gmail.com>
2022-12-08 20:58:56 +0900

    Get secret json from S3 for AWS Lambda

--- remo/spreadsheet.py
+++ remo/spreadsheet.py
@@ -1,12 +1,17 @@
+import json
+import os
 from datetime import datetime
 from pathlib import Path
+from typing import Optional
 
+import boto3
 import gspread
 import structlog
 from gspread.worksheet import Worksheet
 from oauth2client.service_account import ServiceAccountCredentials
 
 from remo.remo import DeviceTemperature
+from remo.utils import IS_AWS_LAMBDA_RUNTIME
 
 logger = structlog.get_logger(__name__)
 
@@ -15,10 +20,19 @@ def format_datetime(dt: datetime) -> str:
     return dt.strftime("%Y-%m-%dT%H:%M:%S")
 
 
-def get_sheet_client(sheet_filename: str, secret_json_path: Path) -> Worksheet:
+def get_sheet_client(
+    sheet_filename: str, secret_json_path: Optional[Path] = None, secret_json: Optional[dict[str, str]] = None
+) -> Worksheet:
     # use creds to create a client to interact with the Google Drive API
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name(secret_json_path, scope)
+    if secret_json_path is not None and secret_json is not None:
+        raise Exception("both secret_json and secret_json_path cannot be provided at the same time")
+    elif secret_json_path is not None:
+        creds = ServiceAccountCredentials.from_json_keyfile_name(secret_json_path, scope)
+    elif secret_json is not None:
+        creds = ServiceAccountCredentials.from_json_keyfile_dict(secret_json, scope)
+    else:
+        raise Exception("secret_json or secret_json_path must be provided")
     client = gspread.authorize(creds)
     # Find a workbook by name and open the first sheet
     # Make sure you use the right name here.
@@ -41,7 +55,21 @@ def is_temperature_updated(sheet_client: Worksheet, device_temperature: DeviceTe
 def write_sheet(
     env_dict: dict[str, str], device_temperature: DeviceTemperature, verbose: bool = True
 ) -> None:
-    sheet_client = get_sheet_client(env_dict["SHEET_FILENAME"], Path(env_dict["SECRET_JSON_PATH"]))
+    secret_json_path = Path(env_dict["SECRET_JSON_PATH"])
+    if secret_json_path.exists():
+        sheet_client = get_sheet_client(env_dict["SHEET_FILENAME"], secret_json_path=secret_json_path)
+    elif IS_AWS_LAMBDA_RUNTIME:
+        # Try to get secret json from S3 for AWS Lambda
+        try:
+            s3 = boto3.resource("s3")
+            bucket = s3.Bucket(env_dict["AWS_SECRET_JSON_PATH_BUCKET"])
+            secret_json_string = (
+                bucket.Object(env_dict["AWS_SECRET_JSON_PATH_KEY"]).get()["Body"].read().decode("utf-8")
+            )
+            secret_json = json.loads(secret_json_string)
+            sheet_client = get_sheet_client(env_dict["SHEET_FILENAME"], secret_json=secret_json)
+        except Exception as e:
+            raise Exception(f"Failed to get sheet client: {e}")
     if is_temperature_updated(sheet_client, device_temperature):
         row = [device_temperature.val, format_datetime(device_temperature.created_at)]
         index = len(sheet_client.get_all_values()) + 1
